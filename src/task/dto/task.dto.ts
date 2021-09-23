174978960b7bfb7021ac2a0bbb5041a4813e17d2
Rafael Witt <rafawitt@hotmail.com>
2021-09-23 18:38:48 -0300

    Update the main file to activate CORS and the TaskDTO entity

--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -1,7 +1,9 @@
+import { TaskStatus, TaskType } from '../entities/task.entity';
+
 export class TaskDTO {
   id: number;
   title: string;
   description?: string;
-  type: number;
-  status: number;
+  type: TaskType;
+  status: TaskStatus;
 }
